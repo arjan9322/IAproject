c9d67d28cc14198c483b7f70f289a96882d9a1b5
A.J. Geitenbeek <arjan_geitenbeek@hotmail.com>
2013-04-18 13:10:42 +0200

    added project

---
+++ AIproject/AIproject/AIproject/BaseGameEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace AIproject
+{
+    class BaseGameEntity
+    {
+        public int ID { get; set; }
+        public Vector2 pos { get; set; }
+        public float scale { get; set; }
+        public float Bradius { get; set; }
+    }
+}
---
+++ AIproject/AIproject/AIproject/MovingEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace AIproject
+{
+    class MovingEntity : BaseGameEntity
+    {
+
+        public Vector2 velocity { get; set; }
+        public Vector2 heading { get; set; }
+        public float mass { get; set; }
+        public float maxSpeed { get; set; }
+        public float maxForce { get; set; }
+        public float maxTurnRate { get; set; }
+    }
+}
